0d4cb29d95b3057d816fd0bda57eceb8a49fe831
devairftonon <devair.tonon@cantustore.com.br>
2023-03-07 20:42:30 -0300

    ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,7 @@ import express from 'express';
 const app = express();
 
 app.listen(3333, () => console.log("server is running"));
+
+app.get('/', (request, response)=>{
+    return response.json({message : 'Hello cruel world'});
+});
