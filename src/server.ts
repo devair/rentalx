d69a4efcfb99a2f4d70770b04096ebdca7c878df
devairftonon <devair.tonon@cantustore.com.br>
2023-03-08 14:00:21 -0300

    categories routers

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
-
+import { categoriesRoutes } from './routes/categories.routers';
 const app = express();
 
-app.use(express.json);
-
-app.listen(3333, () => console.log("server is running"));
+app.use(express.json());
 
 app.get('/', (request, response)=>{
-    return response.json({message : 'Hello cruel world'});
+
+    return response.status(200).json({message: "Hello cruel world"});
 });
+
+
+app.use(categoriesRoutes);
+
+
+app.listen(3333, () => console.log("server is running"));
